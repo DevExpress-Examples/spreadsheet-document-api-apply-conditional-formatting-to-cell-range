1db0f82ce435b053ee47b844197598892bf0f2fd
DevExpressExampleBot <fakeEmail>
2019-10-09 15:17:06 +0300

    Source auto update [skip ci]

--- VB/ConditionalFormatting_Example/BusinessObjects.vb
+++ VB/ConditionalFormatting_Example/BusinessObjects.vb
@@ -8,77 +8,77 @@ Imports DevExpress.Spreadsheet
 Imports System.ComponentModel
 
 Namespace ConditionalFormatting_Example
-    Public Class SpreadsheetNode
+	Public Class SpreadsheetNode
+'INSTANT VB NOTE: The field groups was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private groups_Renamed As New GroupsOfSpreadsheetExamples()
+'INSTANT VB NOTE: The field owner was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private owner_Renamed As GroupsOfSpreadsheetExamples
 
-        Private groups_Renamed As New GroupsOfSpreadsheetExamples()
+		Public Sub New(ByVal name As String)
+			Me.Name = name
+		End Sub
+		<Browsable(False)>
+		Public ReadOnly Property Groups() As GroupsOfSpreadsheetExamples
+			Get
+				Return groups_Renamed
+			End Get
+		End Property
+		Public Property Name() As String
 
-        Private owner_Renamed As GroupsOfSpreadsheetExamples
+		<Browsable(False)>
+		Public Property Owner() As GroupsOfSpreadsheetExamples
+			Get
+				Return owner_Renamed
+			End Get
+			Set(ByVal value As GroupsOfSpreadsheetExamples)
+				owner_Renamed = value
+			End Set
+		End Property
+	End Class
 
-        Public Sub New(ByVal name As String)
-            Me.Name = name
-        End Sub
-        <Browsable(False)> _
-        Public ReadOnly Property Groups() As GroupsOfSpreadsheetExamples
-            Get
-                Return groups_Renamed
-            End Get
-        End Property
-        Public Property Name() As String
+	Public Class SpreadsheetExample
+		Inherits SpreadsheetNode
 
-        <Browsable(False)> _
-        Public Property Owner() As GroupsOfSpreadsheetExamples
-            Get
-                Return owner_Renamed
-            End Get
-            Set(ByVal value As GroupsOfSpreadsheetExamples)
-                owner_Renamed = value
-            End Set
-        End Property
-    End Class
+		Public Sub New(ByVal name As String, ByVal action As Action(Of IWorkbook))
+			MyBase.New(name)
+			Me.Action = action
+		End Sub
+		Private privateAction As Action(Of IWorkbook)
+		Public Property Action() As Action(Of IWorkbook)
+			Get
+				Return privateAction
+			End Get
+			Private Set(ByVal value As Action(Of IWorkbook))
+				privateAction = value
+			End Set
+		End Property
+	End Class
 
-    Public Class SpreadsheetExample
-        Inherits SpreadsheetNode
+	Public Class GroupsOfSpreadsheetExamples
+		Inherits BindingList(Of SpreadsheetNode)
+		Implements TreeList.IVirtualTreeListData
 
-        Public Sub New(ByVal name As String, ByVal action As Action(Of IWorkbook))
-            MyBase.New(name)
-            Me.Action = action
-        End Sub
-        Private privateAction As Action(Of IWorkbook)
-        Public Property Action() As Action(Of IWorkbook)
-            Get
-                Return privateAction
-            End Get
-            Private Set(ByVal value As Action(Of IWorkbook))
-                privateAction = value
-            End Set
-        End Property
-    End Class
-
-    Public Class GroupsOfSpreadsheetExamples
-        Inherits BindingList(Of SpreadsheetNode)
-        Implements TreeList.IVirtualTreeListData
-
-        Private Sub IVirtualTreeListData_VirtualTreeGetChildNodes(ByVal info As VirtualTreeGetChildNodesInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetChildNodes
-            Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
-            info.Children = obj.Groups
-        End Sub
-        Protected Overrides Sub InsertItem(ByVal index As Integer, ByVal item As SpreadsheetNode)
-            item.Owner = Me
-            MyBase.InsertItem(index, item)
-        End Sub
-        Private Sub IVirtualTreeListData_VirtualTreeGetCellValue(ByVal info As VirtualTreeGetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetCellValue
-            Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
-            Select Case info.Column.Caption
-                Case "Name"
-                    info.CellData = obj.Name
-            End Select
-        End Sub
-        Private Sub IVirtualTreeListData_VirtualTreeSetCellValue(ByVal info As VirtualTreeSetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeSetCellValue
-            Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
-            Select Case info.Column.Caption
-                Case "Name"
-                    obj.Name = CStr(info.NewCellData)
-            End Select
-        End Sub
-    End Class
+		Private Sub IVirtualTreeListData_VirtualTreeGetChildNodes(ByVal info As VirtualTreeGetChildNodesInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetChildNodes
+			Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
+			info.Children = obj.Groups
+		End Sub
+		Protected Overrides Sub InsertItem(ByVal index As Integer, ByVal item As SpreadsheetNode)
+			item.Owner = Me
+			MyBase.InsertItem(index, item)
+		End Sub
+		Private Sub IVirtualTreeListData_VirtualTreeGetCellValue(ByVal info As VirtualTreeGetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetCellValue
+			Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
+			Select Case info.Column.Caption
+				Case "Name"
+					info.CellData = obj.Name
+			End Select
+		End Sub
+		Private Sub IVirtualTreeListData_VirtualTreeSetCellValue(ByVal info As VirtualTreeSetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeSetCellValue
+			Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
+			Select Case info.Column.Caption
+				Case "Name"
+					obj.Name = CStr(info.NewCellData)
+			End Select
+		End Sub
+	End Class
 End Namespace
