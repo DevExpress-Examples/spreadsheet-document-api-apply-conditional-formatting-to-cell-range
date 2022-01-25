39fdc49266b17b28a43bacc9ef107fb191b026df
DevExpressExampleBot <fakeEmail>
2022-01-25 10:55:56 +0300

    Source auto update [skip ci]

--- VB/ConditionalFormatting_Example/BusinessObjects.vb
+++ VB/ConditionalFormatting_Example/BusinessObjects.vb
@@ -1,84 +1,90 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
+Imports System
 Imports DevExpress.XtraTreeList
 Imports DevExpress.Spreadsheet
 Imports System.ComponentModel
 
 Namespace ConditionalFormatting_Example
-	Public Class SpreadsheetNode
-'INSTANT VB NOTE: The field groups was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private groups_Renamed As New GroupsOfSpreadsheetExamples()
-'INSTANT VB NOTE: The field owner was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private owner_Renamed As GroupsOfSpreadsheetExamples
 
-		Public Sub New(ByVal name As String)
-			Me.Name = name
-		End Sub
-		<Browsable(False)>
-		Public ReadOnly Property Groups() As GroupsOfSpreadsheetExamples
-			Get
-				Return groups_Renamed
-			End Get
-		End Property
-		Public Property Name() As String
+    Public Class SpreadsheetNode
 
-		<Browsable(False)>
-		Public Property Owner() As GroupsOfSpreadsheetExamples
-			Get
-				Return owner_Renamed
-			End Get
-			Set(ByVal value As GroupsOfSpreadsheetExamples)
-				owner_Renamed = value
-			End Set
-		End Property
-	End Class
+        Private groupsField As GroupsOfSpreadsheetExamples = New GroupsOfSpreadsheetExamples()
 
-	Public Class SpreadsheetExample
-		Inherits SpreadsheetNode
+        Private ownerField As GroupsOfSpreadsheetExamples
 
-		Public Sub New(ByVal name As String, ByVal action As Action(Of IWorkbook))
-			MyBase.New(name)
-			Me.Action = action
-		End Sub
-		Private privateAction As Action(Of IWorkbook)
-		Public Property Action() As Action(Of IWorkbook)
-			Get
-				Return privateAction
-			End Get
-			Private Set(ByVal value As Action(Of IWorkbook))
-				privateAction = value
-			End Set
-		End Property
-	End Class
+        Public Sub New(ByVal name As String)
+            Me.Name = name
+        End Sub
 
-	Public Class GroupsOfSpreadsheetExamples
-		Inherits BindingList(Of SpreadsheetNode)
-		Implements TreeList.IVirtualTreeListData
+        <Browsable(False)>
+        Public ReadOnly Property Groups As GroupsOfSpreadsheetExamples
+            Get
+                Return groupsField
+            End Get
+        End Property
 
-		Private Sub IVirtualTreeListData_VirtualTreeGetChildNodes(ByVal info As VirtualTreeGetChildNodesInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetChildNodes
-			Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
-			info.Children = obj.Groups
-		End Sub
-		Protected Overrides Sub InsertItem(ByVal index As Integer, ByVal item As SpreadsheetNode)
-			item.Owner = Me
-			MyBase.InsertItem(index, item)
-		End Sub
-		Private Sub IVirtualTreeListData_VirtualTreeGetCellValue(ByVal info As VirtualTreeGetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetCellValue
-			Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
-			Select Case info.Column.Caption
-				Case "Name"
-					info.CellData = obj.Name
-			End Select
-		End Sub
-		Private Sub IVirtualTreeListData_VirtualTreeSetCellValue(ByVal info As VirtualTreeSetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeSetCellValue
-			Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
-			Select Case info.Column.Caption
-				Case "Name"
-					obj.Name = CStr(info.NewCellData)
-			End Select
-		End Sub
-	End Class
+        Public Property Name As String
+
+        <Browsable(False)>
+        Public Property Owner As GroupsOfSpreadsheetExamples
+            Get
+                Return ownerField
+            End Get
+
+            Set(ByVal value As GroupsOfSpreadsheetExamples)
+                ownerField = value
+            End Set
+        End Property
+    End Class
+
+    Public Class SpreadsheetExample
+        Inherits SpreadsheetNode
+
+        Private _Action As Action(Of DevExpress.Spreadsheet.IWorkbook)
+
+        Public Sub New(ByVal name As String, ByVal action As Action(Of IWorkbook))
+            MyBase.New(name)
+            Me.Action = action
+        End Sub
+
+        Public Property Action As Action(Of IWorkbook)
+            Get
+                Return _Action
+            End Get
+
+            Private Set(ByVal value As Action(Of IWorkbook))
+                _Action = value
+            End Set
+        End Property
+    End Class
+
+    Public Class GroupsOfSpreadsheetExamples
+        Inherits BindingList(Of SpreadsheetNode)
+        Implements TreeList.IVirtualTreeListData
+
+        Private Sub VirtualTreeGetChildNodes(ByVal info As VirtualTreeGetChildNodesInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetChildNodes
+            Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
+            info.Children = obj.Groups
+        End Sub
+
+        Protected Overrides Sub InsertItem(ByVal index As Integer, ByVal item As SpreadsheetNode)
+            item.Owner = Me
+            MyBase.InsertItem(index, item)
+        End Sub
+
+        Private Sub VirtualTreeGetCellValue(ByVal info As VirtualTreeGetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeGetCellValue
+            Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
+            Select Case info.Column.Caption
+                Case "Name"
+                    info.CellData = obj.Name
+            End Select
+        End Sub
+
+        Private Sub VirtualTreeSetCellValue(ByVal info As VirtualTreeSetCellValueInfo) Implements TreeList.IVirtualTreeListData.VirtualTreeSetCellValue
+            Dim obj As SpreadsheetNode = TryCast(info.Node, SpreadsheetNode)
+            Select Case info.Column.Caption
+                Case "Name"
+                    obj.Name = CStr(info.NewCellData)
+            End Select
+        End Sub
+    End Class
 End Namespace
--- VB/ConditionalFormatting_Example/Form1.Designer.vb
+++ VB/ConditionalFormatting_Example/Form1.Designer.vb
@@ -1,123 +1,124 @@
-﻿Namespace ConditionalFormatting_Example
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace ConditionalFormatting_Example
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.splitContainer1 = New System.Windows.Forms.SplitContainer()
-			Me.treeList1 = New DevExpress.XtraTreeList.TreeList()
-			Me.treeListColumn1 = New DevExpress.XtraTreeList.Columns.TreeListColumn()
-			Me.button1 = New System.Windows.Forms.Button()
-			CType(Me.splitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.splitContainer1.Panel1.SuspendLayout()
-			Me.splitContainer1.Panel2.SuspendLayout()
-			Me.splitContainer1.SuspendLayout()
-			CType(Me.treeList1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' splitContainer1
-			' 
-			Me.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-			Me.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.splitContainer1.IsSplitterFixed = True
-			Me.splitContainer1.Location = New System.Drawing.Point(0, 0)
-			Me.splitContainer1.Name = "splitContainer1"
-			Me.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
-			' 
-			' splitContainer1.Panel1
-			' 
-			Me.splitContainer1.Panel1.Controls.Add(Me.treeList1)
-			Me.splitContainer1.Panel1MinSize = 0
-			' 
-			' splitContainer1.Panel2
-			' 
-			Me.splitContainer1.Panel2.Controls.Add(Me.button1)
-			Me.splitContainer1.Panel2.Tag = "1"
-			Me.splitContainer1.Size = New System.Drawing.Size(342, 543)
-			Me.splitContainer1.SplitterDistance = 470
-			Me.splitContainer1.TabIndex = 0
-			' 
-			' treeList1
-			' 
-			Me.treeList1.Appearance.FocusedCell.Font = New System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold)
-			Me.treeList1.Appearance.FocusedCell.ForeColor = System.Drawing.Color.FromArgb((CInt((CByte(0)))), (CInt((CByte(64)))), (CInt((CByte(0)))))
-			Me.treeList1.Appearance.FocusedCell.Options.UseFont = True
-			Me.treeList1.Appearance.FocusedCell.Options.UseForeColor = True
-			Me.treeList1.Appearance.FocusedRow.Font = New System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(0)))
-			Me.treeList1.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() { Me.treeListColumn1})
-			Me.treeList1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.treeList1.Location = New System.Drawing.Point(0, 0)
-			Me.treeList1.Name = "treeList1"
-			Me.treeList1.OptionsBehavior.Editable = False
-			Me.treeList1.OptionsView.ShowColumns = False
-			Me.treeList1.OptionsView.ShowIndicator = False
-			Me.treeList1.Size = New System.Drawing.Size(338, 466)
-			Me.treeList1.TabIndex = 0
-			' 
-			' treeListColumn1
-			' 
-			Me.treeListColumn1.Caption = "Name"
-			Me.treeListColumn1.FieldName = "Name"
-			Me.treeListColumn1.Name = "treeListColumn1"
-			Me.treeListColumn1.UnboundType = DevExpress.XtraTreeList.Data.UnboundColumnType.Object
-			Me.treeListColumn1.Visible = True
-			Me.treeListColumn1.VisibleIndex = 0
-			' 
-			' button1
-			' 
-			Me.button1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.button1.Font = New System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte(0)))
-			Me.button1.Location = New System.Drawing.Point(0, 0)
-			Me.button1.Name = "button1"
-			Me.button1.Size = New System.Drawing.Size(338, 65)
-			Me.button1.TabIndex = 0
-			Me.button1.Text = "Run"
-			Me.button1.UseVisualStyleBackColor = True
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.button1.Click += new System.EventHandler(this.button1_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(342, 543)
-			Me.Controls.Add(Me.splitContainer1)
-			Me.Name = "Form1"
-			Me.Text = "Conditional Formatting Examples"
-			Me.splitContainer1.Panel1.ResumeLayout(False)
-			Me.splitContainer1.Panel2.ResumeLayout(False)
-			CType(Me.splitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.splitContainer1.ResumeLayout(False)
-			CType(Me.treeList1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.splitContainer1 = New System.Windows.Forms.SplitContainer()
+            Me.treeList1 = New DevExpress.XtraTreeList.TreeList()
+            Me.treeListColumn1 = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+            Me.button1 = New System.Windows.Forms.Button()
+            CType((Me.splitContainer1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.splitContainer1.Panel1.SuspendLayout()
+            Me.splitContainer1.Panel2.SuspendLayout()
+            Me.splitContainer1.SuspendLayout()
+            CType((Me.treeList1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' splitContainer1
+            ' 
+            Me.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+            Me.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.splitContainer1.IsSplitterFixed = True
+            Me.splitContainer1.Location = New System.Drawing.Point(0, 0)
+            Me.splitContainer1.Name = "splitContainer1"
+            Me.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+            ' 
+            ' splitContainer1.Panel1
+            ' 
+            Me.splitContainer1.Panel1.Controls.Add(Me.treeList1)
+            Me.splitContainer1.Panel1MinSize = 0
+            ' 
+            ' splitContainer1.Panel2
+            ' 
+            Me.splitContainer1.Panel2.Controls.Add(Me.button1)
+            Me.splitContainer1.Panel2.Tag = "1"
+            Me.splitContainer1.Size = New System.Drawing.Size(342, 543)
+            Me.splitContainer1.SplitterDistance = 470
+            Me.splitContainer1.TabIndex = 0
+            ' 
+            ' treeList1
+            ' 
+            Me.treeList1.Appearance.FocusedCell.Font = New System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold)
+            Me.treeList1.Appearance.FocusedCell.ForeColor = System.Drawing.Color.FromArgb((CInt(((CByte((0)))))), (CInt(((CByte((64)))))), (CInt(((CByte((0)))))))
+            Me.treeList1.Appearance.FocusedCell.Options.UseFont = True
+            Me.treeList1.Appearance.FocusedCell.Options.UseForeColor = True
+            Me.treeList1.Appearance.FocusedRow.Font = New System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte((0))))
+            Me.treeList1.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() {Me.treeListColumn1})
+            Me.treeList1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.treeList1.Location = New System.Drawing.Point(0, 0)
+            Me.treeList1.Name = "treeList1"
+            Me.treeList1.OptionsBehavior.Editable = False
+            Me.treeList1.OptionsView.ShowColumns = False
+            Me.treeList1.OptionsView.ShowIndicator = False
+            Me.treeList1.Size = New System.Drawing.Size(338, 466)
+            Me.treeList1.TabIndex = 0
+            ' 
+            ' treeListColumn1
+            ' 
+            Me.treeListColumn1.Caption = "Name"
+            Me.treeListColumn1.FieldName = "Name"
+            Me.treeListColumn1.Name = "treeListColumn1"
+            Me.treeListColumn1.UnboundType = DevExpress.XtraTreeList.Data.UnboundColumnType.[Object]
+            Me.treeListColumn1.Visible = True
+            Me.treeListColumn1.VisibleIndex = 0
+            ' 
+            ' button1
+            ' 
+            Me.button1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.button1.Font = New System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, (CByte((0))))
+            Me.button1.Location = New System.Drawing.Point(0, 0)
+            Me.button1.Name = "button1"
+            Me.button1.Size = New System.Drawing.Size(338, 65)
+            Me.button1.TabIndex = 0
+            Me.button1.Text = "Run"
+            Me.button1.UseVisualStyleBackColor = True
+            AddHandler Me.button1.Click, New System.EventHandler(AddressOf Me.button1_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(342, 543)
+            Me.Controls.Add(Me.splitContainer1)
+            Me.Name = "Form1"
+            Me.Text = "Conditional Formatting Examples"
+            Me.splitContainer1.Panel1.ResumeLayout(False)
+            Me.splitContainer1.Panel2.ResumeLayout(False)
+            CType((Me.splitContainer1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.splitContainer1.ResumeLayout(False)
+            CType((Me.treeList1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private splitContainer1 As System.Windows.Forms.SplitContainer
-		Private treeList1 As DevExpress.XtraTreeList.TreeList
-		Private WithEvents button1 As System.Windows.Forms.Button
-		Private treeListColumn1 As DevExpress.XtraTreeList.Columns.TreeListColumn
-	End Class
+#End Region
+        Private splitContainer1 As System.Windows.Forms.SplitContainer
+
+        Private treeList1 As DevExpress.XtraTreeList.TreeList
+
+        Private button1 As System.Windows.Forms.Button
+
+        Private treeListColumn1 As DevExpress.XtraTreeList.Columns.TreeListColumn
+    End Class
 End Namespace
-
